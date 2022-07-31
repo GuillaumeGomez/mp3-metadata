fc5f6e9360a207d1ee85375958fd264ceb80ead2
Guillaume Gomez <guillaume1.gomez@gmail.com>
2022-07-31 15:01:14 +0200

    Fix formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,6 @@
-pub use enums::{
-    ChannelType,
-    Copyright,
-    CRC,
-    Emphasis,
-    Error,
-    Genre,
-    Layer,
-    Status,
-    Version,
-};
-pub use metadata::{
-    read_from_file,
-    read_from_slice,
-};
-pub use types::{
-    AudioTag,
-    Frame,
-    MP3Metadata,
-    OptionalAudioTags,
-    Url,
-};
+pub use enums::{ChannelType, Copyright, Emphasis, Error, Genre, Layer, Status, Version, CRC};
+pub use metadata::{read_from_file, read_from_slice};
+pub use types::{AudioTag, Frame, MP3Metadata, OptionalAudioTags, Url};
 
 mod consts;
 mod enums;
